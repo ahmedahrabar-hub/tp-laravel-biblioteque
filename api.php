6f812a827e76e19874cfbed13fb24bba6a9a7481
ahmedahrabar-hub <ahmedahrabar@gmail.com>
2026-03-11 11:55:50 +0000

    Add files via upload

---
+++ api.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoanController;
+
+Route::apiResource('loans', LoanController::class);
+Route::patch('loans/{id}/return', [LoanController::class, 'return']);
